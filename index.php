139bf07d9ee7c73733d98a3dba45cf6818041bed
dfduff <dfduff@wpi.edu>
2018-09-19 13:04:21 -0400

    formatting banners

--- index.php
+++ index.php
@@ -44,13 +44,13 @@ $dataPoints = array(
     </script>
 </head>
 <body>
-    <h2 align = "center"> You've just been jebaited into taking a fat rip! Now enjoy the rest of this shit</h2>
     <div class ="leftbanner">
         <img src="bgsmoke.jpg"/>
     </div>
-
-    <div class="chartCont" id="chartContainer" style="height: 370px; width: 100%;"></div>
-
+    <div class = chartCont">
+        <h2 align = "center"> You've just been jebaited into taking a fat rip!</h2>
+        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    </div>
     <div class ="rightbanner">
         <img src="bgsmoke.jpg"/>
     </div>
