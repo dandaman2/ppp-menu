95bc57e80ee1770e1d81ee98aaf0fe3c9fae4088
dandaman2 <dfduff@wpi.edu>
2018-09-20 21:36:18 -0400

    text formatting

--- index.php
+++ index.php
@@ -17,10 +17,15 @@ $dataPoints = array(
     <div class ="leftbanner">
         <img src="bgsmoke.jpg" height = "100%"/>
     </div>
-    <div class = "chartCont">
+    <div class = "mainBody">
         <h1 align = "center">Welcome to RipAFat.Cloud!!</h1>
         <h2 align = "center"> You've just been jebaited into taking a fat rip!</h2>
         <h3>What is RipAFat.Cloud?</h3>
+        <p>RipAFat.Cloud is currently an iterative development platform meant for educating the masses about
+        the greatest plant on God's greenest planet: that dank kush!</p>
+        <p>Now you may be saying "But I already know that marijuana is Satan's spinach. The Devil's Lettuce!</p>
+        <p>While this statement may not be wrong, perhaps we can rip a fat cloud in your mind, and delve into the
+        reasoning behind it's widepsread, and increasingly casual use. </p>
         <div id="chartContainer" style="height: 370px;"></div>
     </div>
     <div class ="rightbanner">
