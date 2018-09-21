2133919555821d58fc7f13095f1704bf33997e79
dandaman2 <dfduff@wpi.edu>
2018-09-20 21:11:05 -0400

    Balancing text

--- index.php
+++ index.php
@@ -46,9 +46,7 @@ $dataPoints = array(
     <div class = chartCont">
         <h1 align = "center">Welcome to RipAFat.Cloud!!</h1>
         <h2 align = "center"> You've just been jebaited into taking a fat rip!</h2>
-
-        <h3> What is Ripafat.cloud? </h3>
-        <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+        <div id="chartContainer" style="height: 370px;"></div>
     </div>
     <div class ="rightbanner">
         <img src="bgsmoke.jpg"/>
